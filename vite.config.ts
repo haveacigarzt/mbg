46eb6af1a82698724186e6d93c751f9451b10e0b
Pramana Afriandy <76240438+haveacigarzt@users.noreply.github.com>
2026-05-31 15:47:59 +0700

    fix: update API proxy target to use the correct IP address

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4040",
+        target: "http://192.168.1.10:4040",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
